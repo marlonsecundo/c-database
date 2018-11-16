b5ce374d9dd6963f2fde765def1d74ad227f76fb
unknown <lucasviniciussd2@gmail.com>
2018-11-16 10:38:20 -0300

    Finished DeleteTabel

--- DeleteTabel.c
+++ DeleteTabel.c
@@ -2,12 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-void deleteTabel(char erase[100])
+void deleteTabel()
 {
+    char erase[100];
+    printf("Qual tabela deletar?\n");
+    scanf("%s", &erase);
     char str[100];
     
-    strcat(str, "\n");
-    FILE *Erase = fopen("Tabels", "r+");
+    strcat(erase, "\n");
+    FILE *Erase = fopen("Tabels", "r");
+    FILE *Aux = fopen("Help", "w");
     fseek(Erase, 0, SEEK_SET);
     while (fgets(str, sizeof str, Erase) != NULL)
     {
@@ -16,9 +20,24 @@ void deleteTabel(char erase[100])
         }
         else
         {
-            fprintf(Erase, str);
+            fprintf(Aux, str);
         }
     }
     fclose(Erase);
-    return 0;
+    fclose(Aux);
+    
+    FILE *Done = fopen("Tabels", "w+");
+    FILE *A = fopen("Help", "r");
+
+    fseek(A, 0, SEEK_SET);
+    while (fgets(str, sizeof str, A) != NULL)
+    {
+        fprintf(Done, str);
+    }
+    fclose(A);
+
+    remove("Help");
+    remove(erase);
+
+    fclose(Done);
 }
--- SetTabel.c
+++ SetTabel.c
@@ -2,9 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "DefineTabel.c"
 
 void setTabel(Tabel tabel)
 {
+    int i;
+    int count = sizeof(tabel.columns)/sizeof(Column*);
+    FILE *set = fopen(tabel.name, "w");
+    fprintf(set, "Colunas [\n");
+    for (i = 0; i < count; i++)
+    {
+        fprintf(set, "{ Name:%s Type:%d }\n", tabel.columns[i].name, tabel.columns[i].type);
+    }
+    fprintf(set, "]\n\n");
 
+    fprintf(set, "Chave Primaria:%d\n\n", tabel.primary);
+
+    fprintf(set, "Data\n");
+    
+    fprintf(set, "{ ");
+    
+    
+    fprintf(set, "}");
 }
