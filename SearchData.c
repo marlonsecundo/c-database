46558559b9a08ed95deb16e45f922f8cc4db935f
Lucas Vinicius Sales Dantas <lucasvinicius@A307-06L.imd.local>
2018-11-26 09:52:58 -0300

    Added search data

--- SearchData.c
+++ SearchData.c
@@ -7,7 +7,10 @@ void searchData(Tabel tabel)
 {
     int var = 1;
     char colname[100];
+    int value;
+    int searchValue;
     int i;
+    char search[200];
     printf("As colunas existentes: \n");
     for(i = 0; i < tabel.colLength; i++)
     {
@@ -21,41 +24,90 @@ void searchData(Tabel tabel)
         if (strcmp(colname,tabel.columns[i].name) == 0)
             break;
     }
-    FILE *search = fopen(tabel.name, "r");
-    while(var>0 && var<6)
-    {
-        printf("---- Selecione o estilo de pesquisa ----\n");
-        printf("1 - Pesquisar valores maiores que o informado\n");
-        printf("2 - Pesquisar valores maiores ou iguais ao informado\n");
-        printf("3 - Pesquisar valores iguais ao informado\n");
-        printf("4 - Pesquisar valores menores que o informado\n");
-        printf("5 - Pesquisar valores menores ou iguais ao informado\n");
-        printf("6 - Pesquisar valores proximos ao informado\n");
-        printf("Outro valor - Encerrar pesquisa\n");
-        scanf("%d", &var);
+    FILE *fileSearch = fopen(tabel.name, "r");
+    
+    printf("---- Selecione o estilo de pesquisa ----\n");
+    printf("1 - Pesquisar valores maiores que o informado\n");
+    printf("2 - Pesquisar valores maiores ou iguais ao informado\n");
+    printf("3 - Pesquisar valores iguais ao informado\n");
+    printf("4 - Pesquisar valores menores que o informado\n");
+    printf("5 - Pesquisar valores menores ou iguais ao informado\n");
+    printf("6 - Pesquisar valores proximos ao informado\n");
+    printf("Outro valor - Encerrar pesquisa\n");
+    scanf("%d", &var);
+        
+    printf("Digite o valor para a pesquisa: ");
+    scanf("%d", &value);
 
-        switch(var)
-        {
-            case 1:
-                
-                break;
-            case 2:
-                
-                break;
-            case 3:
-                
-                break;
-            case 4:
-                
-                break;
-            case 5:
-                
-                break;
-            case 6:
-                
-                break;
-            default:
-                break;
-        }
+    strcat(search, " ");
+    strcat(search, colname);
+    strcat(search, ":");
+    strcat(search, "%d");
+    strcat(search, " ");
+        
+    switch(var)
+    {
+        case 1:
+            for (i = 0; i < table.dataLength; i++)
+            {
+                sscanf(table.data[i].value, search, searchValue);
+                if (searchValue > value)
+                {
+                    printf(table.data[i].value);
+                }
+            }
+            break;
+        case 2:
+            for (i = 0; i < table.dataLength; i++)
+            {
+                sscanf(table.data[i].value, search, searchValue);
+                if (searchValue >= value)
+                {
+                    printf(table.data[i].value);
+                }
+            }
+            break;
+        case 3:
+            for (i = 0; i < table.dataLength; i++)
+            {
+                sscanf(table.data[i].value, search, searchValue);
+                if (searchValue == value)
+                {
+                    printf(table.data[i].value);
+                }
+            }
+            break;
+        case 4:
+            for (i = 0; i < table.dataLength; i++)
+            {
+                sscanf(table.data[i].value, search, searchValue);
+                if (searchValue < value)
+                {
+                    printf(table.data[i].value);
+                }
+            } 
+            break;
+        case 5:
+            for (i = 0; i < table.dataLength; i++)
+            {
+                sscanf(table.data[i].value, search, searchValue);
+                if (searchValue <= value)
+                {
+                    printf(table.data[i].value);
+                }
+            }
+            break;
+        case 6:
+            for (i = 0; i < table.dataLength; i++)
+            {
+                sscanf(table.data[i].value, search, searchValue);
+                if (searchValue < value+5 && searchValue > value-5)
+                {
+                    printf(table.data[i].value);
+                }
+            }
+            break;
+        default:
+            break;
     }
 }
