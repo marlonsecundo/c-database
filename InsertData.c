ecc013f6504a4fa9c262437c442232850b8c9166
Marlon Secundo <marlon_secundo@outlook.com>
2018-11-30 09:16:53 -0300

    Updated Insert Data

--- InsertData.c
+++ InsertData.c
@@ -10,6 +10,7 @@ Tabel InsertData(Tabel tabel)
     char data[300] = "";
 
     char value[50] = "";
+    char pkValue[100] = "";
 
     strcat(data, "{");
 
@@ -28,7 +29,7 @@ Tabel InsertData(Tabel tabel)
         case 2:
             sscanf("Float", "%s", type);
             break;
-        case 4:
+        case 3:
             sscanf("Double", "%s", type);
         }
 
@@ -36,12 +37,39 @@ Tabel InsertData(Tabel tabel)
         printf("Digite o valor:");
         scanf("%s", value);
 
+        if (i == tabel.primary)
+            strcpy(pkValue, value);
+
         strcat(data, tabel.columns[i].name);
         strcat(data, ":");
         strcat(data, value);
     }
 
     strcat(data, " }\n");
+
+    // Verificação de PK
+    char *search = NULL;
+    char scanValue[100] = "";
+    char searchValue[100] = "";
+    char colName[100] = "";
+
+    strcat(colName, tabel.columns[tabel.primary].name);
+    strcat(colName, ":");
+
+    strcat(scanValue, colName);
+    strcat(scanValue, "%s");
+
+    for (int i = 0; i < tabel.dataLength - 1; i++)
+    {
+        search = strstr(tabel.data[i].value, colName);
+        sscanf(search, scanValue, searchValue);
+
+        if (strcmp(searchValue, pkValue) == 0)
+        {
+            printf("\nErro:A chave primaria ja existe\n\n");
+            return tabel;
+        }
+    }
 
     tabel.dataLength += 1;
 
